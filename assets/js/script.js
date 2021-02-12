0a26b8ff8cb4d17bccc8263709481b608a22b658
Kimberly Roque <kimroque@gmail.com>
2021-02-12 01:50:03 -0800

    conditional statement added to select first and second cards to reveal images and compare matches

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,18 @@ function initializeApp() {
 }
 
 function handleCardClick(event) {
-  $(event.currentTarget).addClass("hidden")
-  var selectFirstCard = $('.face')
-  if (selectFirstCard) {
-    console.log(selectFirstCard);
-    var img1 = selectFirstCard.siblings().css("background-image");
-    console.log('first image:', img1)
-  } else {
-    selectSecondCard = $(event.currentTarget);
-    var img2 = secondCardClicked.siblings().css("background-image");
-    console.log('second image', img2);
+  $(event.currentTarget).addClass("hidden");
+  if (!firstCardClicked) {
+    firstCardClicked = $(event.currentTarget)
+    var img1 = firstCardClicked.css("background-image");
+    console.log('image 1:', img1)
+  }
+  else if (firstCardClicked && secondCardClicked === null) {
+    secondCardClicked = $(event.currentTarget);
+    var img2 = secondCardClicked.css("background-image");
+    console.log('image 2:', img2);
+  }
+  if (img1 === img2) {
+    console.log('cards match');
   }
 }
