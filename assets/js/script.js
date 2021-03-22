6a7fbe573987dfe2d049df001b7c7951a3bff72d
Kimberly Roque <kimroque@gmail.com>
2021-03-21 17:28:48 -0700

    revisedhandleClick function to find matches and flip unmatched cards after 2 seconds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var firstCardClicked = null;
 var secondCardClicked = null;
+var firstCardClasses;
+var secondCardClasses;
 var gameCards = document.getElementById('gameCards');
-var maxMatches = 10;
+var maxMatches = 2;
 var matches = 0;
 
 gameCards.addEventListener("click", handleClick);
@@ -12,12 +14,49 @@ function handleClick(event) {
     return;
   }
 
-  else if (firstCardClicked && secondCardClicked === null) {
-    secondCardClicked = $(event.currentTarget);
-    var img2 = secondCardClicked.css("background-image");
-    console.log('image 2:', img2);
-  }
-  if (img1 === img2) {
-    console.log('cards match');
+  if (!firstCardClicked) {
+    firstCardClicked =  event.target;
+    firstCardClasses = firstCardClicked.previousElementSibling.classList.value;
+    firstCardClicked.classList.add("hidden");
+    console.log('first', firstCardClasses);
+  } else {
+    secondCardClicked = event.target;
+    secondCardClasses = secondCardClicked.previousElementSibling.classList.value;
+    secondCardClicked.classList.add("hidden");
+    gameCards.removeEventListener("click", handleClick);
+
+    setTimeout(function(){
+      gameCards.addEventListener("click", handleClick);
+    }, 2000 );
+
+    console.log('second', firstCardClasses)
+
+    if (firstCardClasses === secondCardClasses){
+      matches += 1;
+      console.log('matches', matches);
+      if (matches === maxMatches){
+        console.log("You Won!!!");
+        var modal = document.createElement("div");
+        modal.classList.add("modal-overlay");
+        modal.textContent = "You Win";
+        var container = document.querySelector(".container");
+        var body = document.querySelector('body');
+        body.insertAdjacentElement('afterbegin', modal);
+      }
+      firstCardClicked = null;
+      secondCardClicked = null;
+      console.log('match success');
+      return;
+    } else {
+      setTimeout( function(){
+        gameCards.removeEventListener("click", handleClick);
+        firstCardClicked.classList.remove("hidden");
+        secondCardClicked.classList.remove("hidden");
+        gameCards.addEventListener("click", handleClick);
+        firstCardClicked = null;
+        secondCardClicked = null;
+      }, 2000);
+      console.log('no match');
+    }
   }
 }
